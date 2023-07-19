75217af5d986f7b0852bd34c7e3fc8e2d451922c
qwqshq <qwqshq@gmail.com>
2023-07-19 23:00:49 +0800

    Try to launch the QN_Map

---
+++ map/config.js
@@ -0,0 +1,43 @@
+/** 更多样式 http://developer.baidu.com/map/custom/list.htm */
+var MAP_STYLE = "grayscale";
+
+var DATA = {
+	"青岛" : {
+		"山东大学（青岛校区）" : ["A", "B"],
+		"青岛海洋大学" : ["C", "D"]
+	},
+
+	"济南" : {
+		"山东大学" : ["E", "F", "G"]
+	},
+
+	"上海" : {
+		"复旦大学" : ["H", "I"]
+	},
+
+	"北京" : {
+		"清华大学" : ["J", "K"]
+	},
+
+	"兰州" : {
+		"兰州大学" : ["L", "M", "N"]
+	},
+
+	"武汉" : {
+		"华中科技大学" : ["O", "P", "Q"]
+	}
+};
+
+var SPEC_POS = {
+	"univ_C" : [121.597479, 31.185356]
+};
+
+var TEL = {
+};
+
+var MAP_TITLE = "临沂一中19级39班毕业去向";
+
+var ABOUT = {
+	"作者" : ["qiqi"],
+	"框架" : ["<a href='https://github.com/yuehaowang/irmap' target='_blank'>irmap</a>", "Bootstrap", "百度地图"]
+};
